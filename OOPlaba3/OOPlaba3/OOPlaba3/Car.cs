b32cf8aec5b3eafaeb0d8a20aeb06283cc2675f4
kalilask4 <kalilask4@gmail.com>
2021-02-06 03:03:02 +0300

    added class Car (many to many)(agregation)

---
+++ OOPlaba3/OOPlaba3/OOPlaba3/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPlaba3
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Number { get; set; }
+        public string Vin { get; set; } //Vehicle Identification Number
+        public bool is_varning { get; set; }
+        public bool is_Sto_registed { get; set; }
+        //+ class Engine engine;
+        //+ class Driver driver;
+
+
+        
+
+    }
+}
--- OOPlaba3/OOPlaba3/OOPlaba3/Station.cs
+++ OOPlaba3/OOPlaba3/OOPlaba3/Station.cs
@@ -9,19 +9,25 @@ namespace OOPlaba3
         public string Name { get; set; }
         public string Address { get; set; }
         public int Capasity { get; set; }
-        //+cars array
+        public Car[] cars;
 
         public Station()//string name, string address, int capasity)
         {
             Name = "NonameStation";
             Address = "emptyAddress";
             Capasity = 1;
+            cars = new Car[0];
 
         }
         public override string ToString()
         {
-
             string str = $"Станция технического обслуживания {Name} вместимостью {Capasity}";
+            if (cars.Length < Capasity)
+            {
+                str += " примет авто на диагностику.";
+            }
+            else
+                str += $" загружена. ";   //Загружена. Авто и % от готовности
             return str;
         }
     }
